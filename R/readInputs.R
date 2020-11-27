2a382dea46b16e2874df47f12b911f58826f53e7
jungbt <benjung2514@gmail.com>
2020-11-27 14:18:58 -0500

    Update readInputs.R
    CNVID in penncnv and CandCnvFlanks do not match due to ext.

--- R/readInputs.R
+++ R/readInputs.R
@@ -67,6 +67,7 @@ readInputs <- function(args){
                                             paste("BAF", c("F", "M", "O"), sep = "_"),
                                             paste("GENO", c("F", "M", "O"), sep = "_"), "Origin", "CNVID")) %>%
       dplyr::mutate(CNVID = gsub("_flanks_", "_", CNVID)) %>%
+      dplyr::mutate(CNVID = gsub(".out", "", CNVID)) %>%
       dplyr::left_join(cnvProbes, by = c("CNVID", "Name")) %>%
       dplyr::mutate(locus = ifelse(is.na(locus), "flank", locus)) %>%
       dplyr::filter(CNVID %in% penncnv$CNVID))
